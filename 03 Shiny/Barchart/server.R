5f7345148f68b4cbb59614ad418610988ea271db
LAPTOP-LNR9KB7E\Alex Lu <alex.luwenhui@utexas.edu>
2017-05-02 21:22:43 -0500

    crosstab/barchart

--- 03 Shiny/Barchart/server.R
+++ 03 Shiny/Barchart/server.R
@@ -7,63 +7,63 @@ require(data.world)
 require(readr)
 require(DT)
 
-claim_types = query(
-  data.world(propsfile = "www/.data.world"),
-  dataset="brauchlen/s-17-edv-project-6", type="sql",
-  query="select distinct Claim_Type as D, Claim_Type as R
-  from medicare_spending")
+file_path = "../../CSVs/WorldHappiness.csv"
+df <- readr::read_csv(file_path) 
+tdf1 = df %>% dplyr::distinct(Region) %>% arrange(Region) %>% dplyr::rename(D = Region)
+tdf2 = df %>% dplyr::distinct(Region) %>% arrange(Region) %>% dplyr::rename(R = Region)
+regions = bind_cols(tdf1, tdf2)
 
-claim_type_list <- as.list(claim_types$D, claim_types$R)
-claim_type_list <- append(list("All" = "All"), claim_type_list)
-#claim_type_list5 <- claim_type_list
+region_list <- as.list(regions$D, regions$R)
+region_list <- append(list("All" = "All"), region_list)
 
 ############################### Start shinyServer Function ####################
 
 shinyServer(function(input, output) {   
   
-  cons_states = c("AL","AK","ID","KS","MS", "CA","MN","OR","RI","WA")
+  #cons_states = c("AL","AK","ID","KS","MS", "CA","MN","OR","RI","WA")
   
   # These widgets are for the Barcharts tab.
-  output$claim_types2 <- renderUI({selectInput("selectedClaim_Types", "Choose claim types:", claim_type_list, multiple = TRUE, selected='All') })
+  output$regions2 <- renderUI({selectInput("selectedRegions", "Choose regions:", region_list, multiple = TRUE, selected='All') })
   
   # Begin Barchart Tab ------------------------------------------------------------------
   
-  df1 <- eventReactive(input$click2, {
-    if(input$selectedClaim_Types == 'All') claim_type_list <- input$selectedClaim_Types
-    else claim_type_list <- append(list("Skip" = "Skip"), input$selectedClaim_Types)
-    
-    tdf = query(
-      data.world(propsfile = "www/.data.world"),
-      dataset="brauchlen/s-17-edv-project-6", type="sql",
-      query="select State, Claim_Type, avg(Avg_Spending_Per_Episode_State) as avg_spending
-      from medicare_spending_per_capita
-      where (? = 'All' or Claim_Type in (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?))
-      and (State in ('AL','AK','ID','KS','MS', 'CA','MN','OR','RI','WA'))
-      and (Claim_Type not in ('Total'))
-      group by State, Claim_Type",
-      queryParameters = claim_type_list
-    ) # %>% View()
-    
-    # The following two lines mimic what can be done with Analytic SQL. Analytic SQL does not currently work in data.world.
-    tdf2 = tdf %>% group_by(State) %>% summarize(state_avg_spending = mean(avg_spending))
-    dplyr::inner_join(tdf, tdf2, by = "State")
+  dfbc1 <- eventReactive(input$click2, {
+      if(input$selectedRegions == 'All') region_list <- input$selectedRegions
+      else region_list <- append(list("Skip" = "Skip"), input$selectedRegions)
+      
+      file_path = "../../CSVs/status_sector_pop_income.csv"
+      df <- readr::read_csv(file_path) 
+      file_path = "../../CSVs/WorldHappiness.csv"
+      happiness <- readr::read_csv(file_path) 
+      #names(happiness) <- gsub(" ","",names)
+      joined <- dplyr::inner_join(df, happiness, by = "Country")
+      tdf <- joined %>% dplyr::select(Region, Income.Class, Happiness.Score, Happiness.Rank) %>% 
+              dplyr::filter(Income.Class %in% c('High Income','Upper Middle Income','Lower Middle Income','Low Income')) %>% 
+              dplyr::group_by(Income.Class, Region) %>% 
+              dplyr::summarise(avg_scores = mean(Happiness.Score), avg_ranks = mean(Happiness.Rank),
+                               kpi = if_else(avg_ranks <= 33, '03 Low', if_else(avg_ranks <= 67, '02 Medium', '01 High')))
+      
+      # The following two lines mimic what can be done with Analytic SQL. Analytic SQL does not currently work in data.world.
+      tdf2 <- tdf %>% group_by(Income.Class) %>% summarize(window_avg_scores = mean(avg_scores))
+      dplyr::inner_join(tdf, tdf2, by = "Income.Class")
   })
-  output$barchartData1 <- renderDataTable({DT::datatable(df1(),
+  output$barchartData1 <- renderDataTable({DT::datatable(dfbc1(),
                                                          rownames = FALSE,
                                                          extensions = list(Responsive = TRUE, FixedHeader = TRUE) )
   })
-  output$barchartPlot1 <- renderPlot({ggplot(df1(), aes(x=Claim_Type, y=avg_spending)) +
-      scale_y_continuous(labels = scales::comma) + # no scientific notation
-      theme(axis.text.x=element_text(angle=0, size=12, vjust=0.5)) + 
-      theme(axis.text.y=element_text(size=12, hjust=0.5)) +
-      geom_bar(stat = "identity") + 
-      facet_wrap(~State, ncol=1) + 
-      coord_flip() + 
-      # Add sum_sales, and (sum_sales - window_avg_sales) label.
-      geom_text(mapping=aes(x=Claim_Type, y=avg_spending, label=round(avg_spending)),colour="black", hjust=-.5) +
-      geom_text(mapping=aes(x=Claim_Type, y=avg_spending, label=round(avg_spending - state_avg_spending)),colour="blue", hjust=-2) +
-      # Add reference line with a label.
-      geom_hline(aes(yintercept = round(state_avg_spending)), color="red") +
-      geom_text(aes( -1, state_avg_spending, label = state_avg_spending, vjust = -.5, hjust = -.25), color="red")
+  
+  output$barchartPlot1 <- renderPlot({ggplot(dfbc1(), aes(x=Region, y=avg_scores, fill=kpi)) +
+          scale_y_continuous(labels = scales::comma) + # no scientific notation
+          theme(axis.text.x=element_text(angle=0, size=12, vjust=0.5)) + 
+          theme(axis.text.y=element_text(size=12, hjust=0.5)) +
+          geom_bar(stat = "identity") + 
+          facet_wrap(~Income.Class, ncol=1) + 
+          coord_flip() + 
+          # Add sum_sales, and (sum_sales - window_avg_sales) label.
+          geom_text(mapping=aes(x=Region, y=avg_scores, label=round(avg_scores)),colour="black", hjust=-.5) +
+          geom_text(mapping=aes(x=Region, y=avg_scores, label=round(avg_scores - window_avg_scores)),colour="blue", hjust=-2) +
+          # Add reference line with a label.
+          geom_hline(aes(yintercept = round(window_avg_scores)), color="red") +
+          geom_text(aes( -1, window_avg_scores, label = window_avg_scores, vjust = -.5, hjust = -.25), color="red")
   })
 })
--- 03 Shiny/Crosstab/server.R
+++ 03 Shiny/Crosstab/server.R
@@ -10,68 +10,42 @@ require(DT)
 
 shinyServer(function(input, output) { 
 # These widgets are for the Crosstabs tab.
-  online1 = reactive({input$rb1})
+  
   KPI_Low = reactive({input$KPI1})     
   KPI_Medium = reactive({input$KPI2})
   
 # Begin Crosstab Tab ------------------------------------------------------------------
   df1 <- eventReactive(input$click1, {
-      if(online1() == "SQL") {
-        print("Getting from data.world")
-        query(
-            data.world(propsfile = "www/.data.world"),
-            dataset="brauchlen/s-17-edv-project-5", type="sql",
-            query="SELECT ProviderState, MeasureDescription,
-                   avg(AdjustedScore) as avg_adjusted, avg(ExpectedScore) as avg_expected, 
-                   avg(AdjustedScore) / avg(ExpectedScore) as ratio,
-                    
-                   case
-                   when avg(AdjustedScore) / avg(ExpectedScore) < ? then '03 Low'
-                   when avg(AdjustedScore) / avg(ExpectedScore) < ? then '02 Medium'
-                   else '01 High'
-                   end AS kpi
-                    
-                   FROM rehospitalization  
-                   group by ProviderState, MeasureDescription
-                   order by ProviderState", 
-            queryParameters = list(KPI_Low(), KPI_Medium())
-          ) }
+      file_path = "../../CSVs/status_sector_pop_income.csv"
+      df <- readr::read_csv(file_path) 
+      file_path = "../../CSVs/WorldHappiness.csv"
+      happiness <- readr::read_csv(file_path) 
+      #names(happiness) <- gsub(" ","_",names)
+      names
+      joined <- dplyr::inner_join(df, happiness, by = "Country")
+        
+      df1 <- joined %>% dplyr::select(Total.employment.in.agriculture..thousands.,
+                                      Total.employment.in.industry..thousands.,
+                                      Total.employment.in.services..thousands.,
+                                      Region,Happiness.Score,Happiness.Rank)
       
-      else {
-        print("Getting from csv")
-        file_path = "www/rehospitalization.csv"
-        df2 <- readr::read_csv(file_path)
-        
-        df2_1 <- df2 %>% 
-            dplyr::select(ProviderState, MeasureDescription, AdjustedScore, ExpectedScore)
-        
-        na2zero <- function (x) {
-            x[is.na(x)] <- 0
-            return(x)}
-        
-        measures <- c("ExpectedScore", "AdjustedScore")
-        
-        if( length(measures) > 1) {
-            for(m in measures) {
-                df2_1[m] <- data.frame(lapply(df2_1[m], na2zero))
-                df2_1[m] <- data.frame(lapply(df2_1[m], function(x) as.numeric(as.character(x))))}}
-        
-        df2_1 %>%
-            dplyr::group_by(ProviderState, MeasureDescription) %>% 
-            dplyr::summarise(avg_adjusted = mean(AdjustedScore), 
-                             avg_expected = mean(ExpectedScore), 
-                             ratio = mean(AdjustedScore)/mean(ExpectedScore),
-                             kpi = if_else(ratio <= KPI_Low(), '03 Low',
-                                   if_else(ratio <= KPI_Medium(), '02 Medium', '01 High'))) %>%
-            dplyr::arrange(ProviderState)
-        }
+      df2 <- df1 %>% dplyr::mutate(MainIndustry = if_else(Total.employment.in.agriculture..thousands.>=Total.employment.in.industry..thousands. & Total.employment.in.agriculture..thousands.>=Total.employment.in.services..thousands., 'Agriculture', if_else(Total.employment.in.industry..thousands.>=Total.employment.in.agriculture..thousands. & Total.employment.in.industry..thousands.>=Total.employment.in.services..thousands., 'Industry', 'Services')))
+      
+      df2 %>% dplyr::select(Region, Happiness.Score, Happiness.Rank, MainIndustry) %>% 
+                     dplyr::group_by(MainIndustry, Region) %>% 
+                     dplyr::summarise(avg_scores = round(mean(Happiness.Score),2), avg_ranks = round(mean(Happiness.Rank),2), 
+                        kpi = if_else(avg_ranks <= KPI_Low(), '03 Low', if_else(avg_ranks <= KPI_Medium(), '02 Medium', '01 High')))
       })
   
   output$data1 <- renderDataTable({DT::datatable(df1(), rownames = FALSE,
-                                extensions = list(Responsive = TRUE, FixedHeader = TRUE))})
+                                extensions = list(Responsive = TRUE, FixedHeader = TRUE))
+      })
   
   output$plot1 <- renderPlot({ggplot(df1()) + 
-    geom_text(aes(x=MeasureDescription, y=ProviderState, label=avg_adjusted), size=6) +
-    geom_tile(aes(x=MeasureDescription, y=ProviderState, fill=kpi), alpha=0.50)})
+    geom_text(aes(x=Region, y=MainIndustry, label=avg_scores), size=6) +
+    geom_tile(aes(x=Region, y=MainIndustry, fill=kpi), alpha=0.50) +
+    theme(axis.text.x=element_text(angle=90, size=12, vjust=0.5)) + 
+    theme(axis.text.y=element_text(size=12, hjust=0.5))
+      })
   
 })
